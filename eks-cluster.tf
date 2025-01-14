d050de16742bc964e5e69a944a6b131445724d66
mikolajwiduch <mikolajwiduch@gmail.com>
2025-01-14 14:31:59 +0100

    properly formatted

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,12 +1,12 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "19.0.4"
 
-  cluster_name = local.cluster_name
+  cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
@@ -20,8 +20,8 @@ module "eks" {
 
       instance_types = ["t3.small"]
 
-      min_size = 1
-      max_size = 3
+      min_size     = 1
+      max_size     = 3
       desired_size = 2
     }
 
@@ -30,8 +30,8 @@ module "eks" {
 
       instance_types = ["t3.small"]
 
-      min_size = 1
-      max_size = 2
+      min_size     = 1
+      max_size     = 2
       desired_size = 1
     }
   }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS region"
- type = string
- default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "clusterName" {
   description = "EKS Cluster"
- type = string
- default = "vpro-tf-eks"
+  type        = string
+  default     = "vpro-tf-eks"
 }
