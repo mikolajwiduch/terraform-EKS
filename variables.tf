bb17c44642be719f8b411174904bb43c194d0ed3
mikolajwiduch <mikolajwiduch@gmail.com>
2025-01-14 14:27:03 +0100

    setup

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+ type = string
+ default = "us-east-1"
+}
+
+variable "clusterName" {
+  description = "EKS Cluster"
+ type = string
+ default = "vpro-tf-eks"
+}
